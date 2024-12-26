73642d93f70730446613f7692b540d7fac4115a0
nummad <nummad222@gmail.com>
2024-12-26 12:36:52 +0500

    commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
-
+const cors = require('cors');
 const app = express();
 const port = 9000;
+
+app.use(cors());
 
 app.use('/', (req, res)=>{
     res.json({message: "Hello from api"});
