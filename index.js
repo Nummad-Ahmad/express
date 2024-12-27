262998a63809e43cd8f0a745f4d2f9b0a19c932b
nummad <nummad222@gmail.com>
2024-12-27 14:55:58 +0500

    commit

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 import express from 'express';
 import cors from 'cors';
-
 const app = express();
 const port = 9000;
 
 // Enable CORS for all routes
 app.use(cors());
+app.use(express.json());
+mongoose.connect(conStr).then(() => console.log("DB")).catch(e => console.log(e));
 
-// Define a GET route for the root path
 app.get('/', (req, res) => {
     res.json({ message: "Hello from API" });
 });
 
 app.get('/bye', (req, res)=>{
     res.json({message: "Bye"});
+})
+
+app.post('/data', (req,res)=>{
+    try{
+        res.json({message: 'Hello'});
+    }catch(e){
+        console.log(e)
+    };
 })
 
 // Start the server
